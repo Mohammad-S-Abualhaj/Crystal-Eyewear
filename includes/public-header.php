1fa0d1cfe6cb631c0607c599d5220f1a821c22fb
Ahmad-Kilani-Dev <40439458+dev-kilani@users.noreply.github.com>
2021-12-03 08:56:27 +0300

    i changed the links to be php 2- cleaned the website from pages not needed 3- made master pages for header and footer

---
+++ includes/public-header.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+
+<!-- Mirrored from template.hasthemes.com/shome/shome/index.php by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 02 Dec 2021 14:25:42 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Shome - Shoes eCommerce Website Template"/>
+    <meta name="keywords" content="footwear, shoes, modern, shop, store, ecommerce, responsive, e-commerce"/>
+    <meta name="author" content="codecarnival"/>
+
+    <title>Shome - Shoes eCommerce Website Template</title>
+
+    <!--== Favicon ==-->
+    <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon" />
+
+    <!--== Google Fonts ==-->
+    <link rel="preconnect" href="https://fonts.googleapis.com/">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400;1,500&amp;display=swap" rel="stylesheet"> 
+
+    <!--== Bootstrap CSS ==-->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+    <!--== Font Awesome Min Icon CSS ==-->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+    <!--== Pe7 Stroke Icon CSS ==-->
+    <link href="assets/css/pe-icon-7-stroke.css" rel="stylesheet" />
+    <!--== Swiper CSS ==-->
+    <link href="assets/css/swiper.min.css" rel="stylesheet" />
+    <!--== Fancybox Min CSS ==-->
+    <link href="assets/css/fancybox.min.css" rel="stylesheet" />
+    <!--== Aos Min CSS ==-->
+    <link href="assets/css/aos.min.css" rel="stylesheet" />
+
+    <!--== Main Style CSS ==-->
+    <link href="assets/css/style.css" rel="stylesheet" />
+
+    <!--[if lt IE 9]>
+    <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<!--wrapper start-->
+<div class="wrapper">
+
+  <!--== Start Header Wrapper ==-->
+  <header class="main-header-wrapper position-relative">
+    <div class="header-top">
+      <div class="container pt--0 pb--0">
+        <div class="row">
+          <div class="col-12">
+            <div class="header-top-align">
+              <div class="header-top-align-start">
+                <div class="desc">
+                  <p>World Wide Completely Free Returns and Free Shipping</p>
+                </div>
+              </div>
+              <div class="header-top-align-end">
+                <div class="header-info-items">
+                  <div class="info-items">
+                    <ul>
+                      <li class="number"><i class="fa fa-phone"></i><a href="tel://0123456789">+00 123 456 789</a></li>
+                      <li class="email"><i class="fa fa-envelope"></i><a href="mailto://demo@example.com">demo@example.com</a></li>
+                      <li class="account"><i class="fa fa-user"></i><a href="account-login.php">Account</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header-middle">
+      <div class="container pt--0 pb--0">
+        <div class="row align-items-center">
+          <div class="col-12">
+            <div class="header-middle-align">
+              <div class="header-middle-align-start">
+                <div class="header-logo-area">
+                  <a href="index.php">
+                    <img class="logo-main" src="assets/img/logo.webp" width="131" height="34" alt="Logo" />
+                    <img class="logo-light" src="assets/img/logo-light.webp" width="131" height="34" alt="Logo" />
+                  </a>
+                </div>
+              </div>
+              <div class="header-middle-align-center">
+                <div class="header-search-area">
+                  <form class="header-searchbox">
+                    <input type="search" class="form-control" placeholder="Search">
+                    <button class="btn-submit" type="submit"><i class="pe-7s-search"></i></button>
+                  </form>
+                </div>
+              </div>
+              <div class="header-middle-align-end">
+                <div class="header-action-area">
+                  <div class="shopping-search">
+                    <button class="shopping-search-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#AsideOffcanvasSearch" aria-controls="AsideOffcanvasSearch"><i class="pe-7s-search icon"></i></button>
+                  </div>
+                  <div class="shopping-wishlist">
+                    <a class="shopping-wishlist-btn" href="shop-wishlist.php">
+                      <i class="pe-7s-like icon"></i>
+                    </a>
+                  </div>
+                  <div class="shopping-cart">
+                    <button class="shopping-cart-btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#AsideOffcanvasCart" aria-controls="offcanvasRightLabel">
+                      <i class="pe-7s-shopbag icon"></i>
+                      <sup class="shop-count">02</sup>
+                    </button>
+                  </div>
+                  <button class="btn-menu" type="button" data-bs-toggle="offcanvas" data-bs-target="#AsideOffcanvasMenu" aria-controls="AsideOffcanvasMenu">
+                    <i class="pe-7s-menu"></i>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="header-area header-default">
+      <div class="container">
+        <div class="row no-gutter align-items-center position-relative">
+          <div class="col-12">
+            <div class="header-align">
+              <div class="header-navigation-area position-relative">
+                <ul class="main-menu nav">
+                  <li class="has-submenu"><a href="#/"><span>Home</span></a>
+                    <ul class="submenu-nav">
+                      <li><a href="index.php"><span>Home One</span></a></li>
+                      <li><a href="index-two.php"><span>Home Two</span></a></li>
+                    </ul>
+                  </li>
+                  <li><a href="about-us.php"><span>About</span></a></li>
+                  <li class="has-submenu"><a href="#/"><span>Pages</span></a>
+                    <ul class="submenu-nav">
+                      <li><a href="account.php"><span>Account</span></a></li>
+                      <li><a href="account-login.php"><span>Login</span></a></li>
+                      <li><a href="account-register.php"><span>Register</span></a></li>
+                      <li><a href="page-not-found.php"><span>Page Not Found</span></a></li>
+                    </ul>
+                  </li>
+                  <li class="has-submenu position-static"><a href="#/"><span>Shop</span></a>
+                    <ul class="submenu-nav submenu-nav-mega column-3">
+                      <li class="mega-menu-item"><a href="#/" class="mega-title"><span>Shop Layout</span></a>
+                        <ul>
+                          <li><a href="shop.php"><span>Shop Left Sidebar</span></a></li>
+                          <li><a href="shop-right-sidebar.php"><span>Shop Right Sidebar</span></a></li>
+                        </ul>
+                      </li>
+                      <li class="mega-menu-item"><a href="#/" class="mega-title"><span>Single Product</span></a>
+                        <ul>
+                          <li><a href="single-normal-product.php"><span>Single Product Normal</span></a></li>
+                          <li><a href="single-group-product.php"><span>Single Product Group</span></a></li>
+                        </ul>
+                      </li>
+                      <li class="mega-menu-item"><a href="#/" class="mega-title"><span>Others Pages</span></a>
+                        <ul>
+                          <li><a href="shop-cart.php"><span>Shopping Cart</span></a></li>
+                          <li><a href="shop-checkout.php"><span>Checkout</span></a></li>
+                          <li><a href="shop-wishlist.php"><span>Wishlist</span></a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li class="has-submenu"><a href="#/"><span>Blog</span></a>
+                    <ul class="submenu-nav submenu-nav-mega">
+                      <li class="mega-menu-item"><a href="#/" class="mega-title">Blog Layout</a>
+                        <ul>
+                          <li><a href="blog-left-sidebar.php">Blog Left Sidebar</a></li>
+                        </ul>
+                      </li>
+                      <li class="mega-menu-item"><a href="#/" class="mega-title">Single Blog</a>
+                        <ul>
+                          <li><a href="blog-details-left-sidebar.php">Blog Details Left Sidebar</a></li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="contact.php"><span>Contact</span></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!--== End Header Wrapper ==-->
+  
