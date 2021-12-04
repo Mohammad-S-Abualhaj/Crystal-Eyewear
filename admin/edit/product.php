6792d0e2b6039fd6dc10919bda9afc02ef4eb570
Sahar Zahran <61621196+Saharzahran@users.noreply.github.com>
2021-12-04 17:34:19 +0200

    edit products (done)

--- admin/edit/product.php
+++ admin/edit/product.php
@@ -73,7 +73,7 @@ if (isset($_GET['product_id'])) {
 											<div class="fv-row mb-7">
 												<label class="d-block fw-bold fs-6 mb-5">Product Image</label>
 												<div class="image-input image-input-outline" data-kt-image-input=true style="background-image: url()">
-													<div class="image-input-wrapper w-125px h-125px" style="background-image: url(<?php echo $product_edited['product_name'];?>)"></div>
+													<div class="image-input-wrapper w-125px h-125px" style="background-image: url(../assets/media/products_images/<?php echo $product_edited["product_image"];?>);"></div>
 													<label class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action=change data-bs-toggle=tooltip title="Change avatar"><i class="bi bi-pencil-fill fs-7"></i> <input type="file" name="image" value="<?php echo $product_edited['product_image'];?>" accept=".png, .jpg, .jpeg" class="form-control-file"> <input type=hidden name="avatar_remove"></label><span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action=cancel data-bs-toggle=tooltip title="Cancel avatar"><i class="bi bi-x fs-2"></i></span> <span class="btn btn-icon btn-circle btn-active-color-primary w-25px h-25px bg-body shadow" data-kt-image-input-action=remove data-bs-toggle=tooltip title="Remove avatar"><i class="bi bi-x fs-2"></i></span>
 												</div>
 												<div class=form-text>Allowed file types: png, jpg, jpeg.</div>
