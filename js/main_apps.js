f845268a9b2817e24f10f62a5723f430acad66e9
Ahmad-Kilani-Dev <40439458+dev-kilani@users.noreply.github.com>
2021-12-06 00:23:03 +0300

    edit on regex

--- js/main_apps.js
+++ js/main_apps.js
@@ -39,7 +39,7 @@ function validateCountry(country) {
 }
 
 function validateFullName(name) {
-    return (/^([\w]{3,})+\s+([\w\s]{3,})+$/i).test(name)
+    return (/^([\w]{3,3})+\s+([\w\s]{3,})+$/i).test(name)
 
 }
 
@@ -49,7 +49,7 @@ function validatePassword(pass) {
 }
 
 function validateUsername(username) {
-    return /^[a-zA-Z0-9_]{5,}[a-zA-Z]+[0-9]*$/.test(username);
+    return /^[a-zA-Z0-9_]{2,}[a-zA-Z]+[0-9]*$/.test(username);
 }
 let emailCheck = false;
 let phoneCheck = false;
