37500172f4c7fd8f3624f668ab2542877bd06e04
Mohammad-S-Abualhaj <mohammad.s.abualhaj@gmail.com>
2021-12-06 16:07:20 +0200

    fix

--- shop.php
+++ shop.php
@@ -145,54 +145,6 @@ if (isset($_GET['sub_category_name'])) {
                               <!--== End product Item ==-->
                            </div>
                         </div>
-                        <div class="tab-pane fade" id="nav-list" role="tabpanel" aria-labelledby="nav-list-tab">
-                           <div class="row">
-                              <div class="col-md-12">
-                                 <!--== Start Product Item ==-->
-                                 <div class="product-item product-list-item">
-                                    <div class="inner-content">
-                                       <div class="product-thumb">
-                                          <a href="single-product.html">
-                                             <img src="assets/img/shop/list-1.webp" width="322" height="360" alt="Image-HasTech">
-                                          </a>
-                                          <div class="product-flag">
-                                             <ul>
-                                                <li class="discount">-10%</li>
-                                             </ul>
-                                          </div>
-                                          <div class="product-action">
-                                             <a class="btn-product-wishlist" href="shop-wishlist.html"><i class="fa fa-heart"></i></a>
-                                             <a class="btn-product-cart" href="shop-cart.html"><i class="fa fa-shopping-cart"></i></a>
-                                             <button type="button" class="btn-product-quick-view-open">
-                                                <i class="fa fa-arrows"></i>
-                                             </button>
-                                             <a class="btn-product-compare" href="shop-compare.html"><i class="fa fa-random"></i></a>
-                                          </div>
-                                          <a class="banner-link-overlay" href="shop.html"></a>
-                                       </div>
-                                       <div class="product-info">
-                                          <div class="category">
-                                             <ul>
-                                                <li><a href="shop.html">Men</a></li>
-                                                <li class="sep">/</li>
-                                                <li><a href="shop.html">Women</a></li>
-                                             </ul>
-                                          </div>
-                                          <h4 class="title"><a href="single-product.html">Leather Mens Slipper</a></h4>
-                                          <div class="prices">
-                                             <span class="price-old">$300</span>
-                                             <span class="sep">-</span>
-                                             <span class="price">$240.00</span>
-                                          </div>
-                                          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem quo, rerum rem soluta quisquam, repellat is deleniti omnis culpa ea quis provident dolore esse, offici modi dolorem nam cum eligendi enim!</p>
-                                          <a class="btn-theme btn-sm" href="shop-cart.html">Add To Cart</a>
-                                       </div>
-                                    </div>
-                                 </div>
-                                 <!--== End prPduct Item ==-->
-                              </div>
-                           </div>
-                        </div>
                      </div>
                   </div>
                </div>
